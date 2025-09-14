f7339af5b2e06d3d83312b511879b12100e00f24
Praveen Gupta <coderpraveengupta@gmail.com>
2025-09-14 16:26:49 +0530

    fix mail issue

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -3,11 +3,22 @@ import { ENV } from "./env.js";
 
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: ENV.EMAIL_FROM,
     pass: ENV.EMAIL_APP_PASSWORD
   },
+  tls: {
+    rejectUnauthorized: false,
+    ciphers: 'SSLv3'
+  },
+  pool: true,
+  maxConnections: 5,
+  maxMessages: 100,
+  rateDelta: 20000,
+  rateLimit: 5
 });
 
 
